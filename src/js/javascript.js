44f4931d6188c9ce522e5f15a0d992505f748c7e
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2025-12-10 18:41:45 +0000

    Change divide function to no zero division

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -8,6 +8,9 @@ function multiply(a, b) {
     return a*b ;
 };
 function divide(a,b) {
+    if (b===0) {
+        return "Cannot divide by 0"
+    }
     return a/b ;
 };
 function operate(operator, a, b) {
